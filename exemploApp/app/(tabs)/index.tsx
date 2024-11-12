55dd113a572bd298cfaafc0ef316354ea4b6581e
JOÃO HENRIQUE STOLF GALEAZZI <joao_galeazzi@estudante.sesisenai.org.br>
2024-11-12 17:19:15 -0300

    comecei do 0

--- exemploApp/app/(tabs)/index.tsx
+++ exemploApp/app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Button } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import React, {useState} from 'react';
 
 export default function HomeScreen() {
+  const[count, setCount] = useState(0);
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -15,37 +16,50 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView>
-        Meu primeiro app alcançou a inteligência máxima
-      </ThemedView>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">( ͡° ͜ʖ ͡°)</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Try diving into the fires of Mordor
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '}
+          <ThemedText type="defaultSemiBold">
+            {Platform.select({
+              ios: 'cmd + d',
+              android: 'cmd + m',
+              web: 'F12'
+            })}
+          </ThemedText>{' '}
+          to open developer tools.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
-          Explore Sauron's famous pits of despair
+          Tap the Explore tab to learn more about what's included in this starter app.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
-          When you're ready, simply walk into Mordor today. Only $9,99
+          When you're ready, run{' '}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
+      <Button title="Teste" onPress={() => setCount(count + 1)}/>
+      <ThemedText type="defaultSemiBold">Contador está em {count}</ThemedText>
+      <Button title="Teste -" onPress={() => setCount(count - 1)}/>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
+    backgroundColor: '#ffaaff',
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
